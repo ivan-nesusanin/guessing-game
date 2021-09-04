2be1c1cc0da8152a5fc8f6a9c8fc5d4a5878ec90
ivan-nesusanin <l75x5@yandex.ru>
2021-09-04 19:52:12 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,34 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
+  constructor(num) {
+    this.num = num;
+  }
 
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+
+  guess() {
+    let mid = Math.round((this.min + this.max) / 2);
+    if (mid > this.num) {
+      this.lower;
+    } else if (mid < this.num) {
+      this.greater;
+    } else {
+      return mid;
     }
+  }
 
-    guess() {
+  lower() {
+    this.min = this.min;
+    this.max = Math.round((this.min + this.max) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = Math.round((this.min + this.max) / 2);
+    this.max = this.max;
+  }
 }
 
 module.exports = GuessingGame;
